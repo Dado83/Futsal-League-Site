7d359fa80c461834eb7d25f00c91e21d324e28ed
Dado83 <83deadpool@gmail.com>
2019-01-18 11:55:28 +0100

    setup results page

--- application/views/results.php
+++ application/views/results.php
@@ -1,12 +1,108 @@
 <table>      
+    <p>2005 godiste</p>
+    <td>kolo</td>
+    <td>domacin</td>
+    <td>gost</td>
+    <td colspan="2">rezultat</td>
+</tr>
+<?php
+foreach ($results5 as $row)
+{
+    echo <<<EOT
+        <tr>
+        <td>$row->m_day</td>
+        <td>$row->home_team</td>
+        <td>$row->away_team</td>
+        <td>$row->goals_home</td>
+        <td>$row->goals_away</td>
+        </tr>
+        EOT;
+}
+?>
+</table>
+<table>      
+    <p>2006 godiste</p>
     <tr>
         <td>kolo</td>
+        <td>domacin</td>
+        <td>gost</td>
+        <td colspan="2">rezultat</td>
     </tr>
     <?php
-    foreach ($results5 as $row)
+    foreach ($results6 as $row)
     {
         echo <<<EOT
         <tr>
+        <td>$row->m_day</td>
+        <td>$row->home_team</td>
+        <td>$row->away_team</td>
+        <td>$row->goals_home</td>
+        <td>$row->goals_away</td>
+        </tr>
+        EOT;
+    }
+    ?>
+</table>
+<table>
+    <p>2007 godiste</p>     
+    <tr>
+        <td>kolo</td>
+        <td>domacin</td>
+        <td>gost</td>
+        <td colspan="2">rezultat</td>
+    </tr>
+    <?php
+    foreach ($results7 as $row)
+    {
+        echo <<<EOT
+        <tr>
+        <td>$row->m_day</td>
+        <td>$row->home_team</td>
+        <td>$row->away_team</td>
+        <td>$row->goals_home</td>
+        <td>$row->goals_away</td>
+        </tr>
+        EOT;
+    }
+    ?>
+</table>
+<table>     
+    <p>2008 godiste</p>
+    <tr>
+        <td>kolo</td>
+        <td>domacin</td>
+        <td>gost</td>
+        <td colspan="2">rezultat</td>
+    </tr>
+    <?php
+    foreach ($results8 as $row)
+    {
+        echo <<<EOT
+        <tr>
+        <td>$row->m_day</td>
+        <td>$row->home_team</td>
+        <td>$row->away_team</td>
+        <td>$row->goals_home</td>
+        <td>$row->goals_away</td>
+        </tr>
+        EOT;
+    }
+    ?>
+</table>
+<table>      
+    <p>2009 godiste</p>
+    <tr>
+        <td>kolo</td>
+        <td>domacin</td>
+        <td>gost</td>
+        <td colspan="2">rezultat</td>
+    </tr>
+    <?php
+    foreach ($results9 as $row)
+    {
+        echo <<<EOT
+        <tr>
+        <td>$row->m_day</td>
         <td>$row->home_team</td>
         <td>$row->away_team</td>
         <td>$row->goals_home</td>
