392ac65ff61037b45bef912bbe316598bd2219a0
Dado83 <83deadpool@gmail.com>
2019-11-09 23:12:04 +0100

    style front page

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,35 @@ $(document).ready(function() {
 	let banner = $('img[alt="www.000webhost.com"]').closest('div');
 	banner.remove();
 
+	let url = window.location.pathname;
+	console.log(url);
+
+	switch (url) {
+		case '/':
+			$('#youthSel li:nth-of-type(1)').css('background', '#4267b2');
+			break;
+		case '/liga/index/2006':
+			$('#youthSel li:nth-of-type(1)').css('background', '#4267b2');
+			break;
+		case '/liga/index/2007':
+			$('#youthSel li:nth-of-type(2)').css('background', '#4267b2');
+			break;
+		case '/liga/index/2008':
+			$('#youthSel li:nth-of-type(3)').css('background', '#4267b2');
+			break;
+		case '/liga/index/2009':
+			$('#youthSel li:nth-of-type(4)').css('background', '#4267b2');
+			break;
+		case '/liga/index/2010':
+			$('#youthSel li:nth-of-type(5)').css('background', '#4267b2');
+			break;
+		case '/liga/rezultati':
+			$('#youthSel li:nth-of-type(6)').css('background', '#4267b2');
+			break;
+		case '/liga/raspored':
+			$('#youthSel li:nth-of-type(7)').css('background', '#4267b2');
+			break;
+	}
+
 	//
 });
