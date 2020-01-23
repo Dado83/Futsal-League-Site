75fac572bac8d9cd54d463d3306253d33930f46f
Dado83 <83deadpool@gmail.com>
2020-01-23 13:55:17 +0100

    add modal form for password change

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,7 +1,22 @@
+<div class="modalForm">
+    <form action="/liga/passwordChange" method="post">
+        <fieldset>
+            <label for="user">Korisnik</label>
+            <input type="text" name="user"><br>
+            <label for="oldPassword">Šifra</label>
+            <input type="password" name="oldPassword"><br>
+            <label for="newPassword">Nova šifra</label>
+            <input type="password" name="newPassword"><br>
+            <input type="submit" value="Promijeni šifru">
+        </fieldset>
+    </form>
+</div>
 <div id="adminCards">
-    <div id="newsLetter"><a href="/liga/bilten"><img src="/images/icons/newsletter.svg" style="width: 100%" />Bilten</a>
+    <div class="adminCards"><a href="/liga/bilten"><img src="/images/icons/newsletter.svg" style="width: 100%" />Bilten</a>
     </div>
-    <div id="metrics"><a href="/liga/metrics"><img src="/images/icons/charts.svg" style="width: 100%" />Metrics</a>
+    <div class="adminCards"><a href="/liga/metrics"><img src="/images/icons/charts.svg" style="width: 100%" />Metrics</a>
+    </div>
+    <div class="adminCards"><a href="/liga/metrics"><img src="/images/icons/pass.svg" style="width: 100%" />Password</a>
     </div>
 </div>
 <table class="admin">
@@ -19,7 +34,7 @@ foreach ($results as $row) {
              <td>$row->m_day</td>
              <td>$row->home_team</td>
              <td>$row->away_team</td>
-             <td><a class='button' href='/liga/brisanjeKola/$row->id' onclick="return confirm('Brišem zadnje kolo?')">Brisi</a></td>
+             <td><a class='button' href='/liga/brisanjeKola/$row->id' onclick="return confirm('Brišem zadnje kolo?')">Briši</a></td>
              </tr>
 EOT;
 }
