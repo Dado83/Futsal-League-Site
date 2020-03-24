82f6e6ce5bdc595f3dff6530c21094bc9c10d50a
Dado83 <83deadpool@gmail.com>
2020-03-24 18:40:06 +0100

    add form element values

--- js/skgameindex.js
+++ js/skgameindex.js
@@ -1,6 +1,11 @@
-document.getElementById('search').addEventListener('click', () =>
-    search_games())
+let form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    let title = form.elements['title'].value ? form.elements['title'].value : ''
+    let author = form.elements['author'].value ? form.elements['author'].value : ''
+    let score = form.elements['score'].value ? form.elements['score'].value : 0
+    let platform = document.querySelector('input[name=platform]:checked') ?
+        document.querySelector('input[name=platform]:checked').id : ''
+    console.log(title + author + score + platform)
+    event.preventDefault()
+})
 
-function search_games() {
-    alert('test')
-}
