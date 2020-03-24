760feac302b8b0910ed09f47fa320656985db6a2
Dado83 <83deadpool@gmail.com>
2020-03-24 22:10:06 +0100

    add date column

--- js/skgameindex.js
+++ js/skgameindex.js
@@ -17,14 +17,17 @@ let json = jsonFile.then((response) => response.json().then((data) => {
     for (let i = 0; i < data.length; i++) {
         gameList[i] = data[i]
         let tr = document.createElement('tr')
+        let td = document.createElement('td')
         let td1 = document.createElement('td')
         let td2 = document.createElement('td')
         let td3 = document.createElement('td')
         let td4 = document.createElement('td')
-        td1.textContent = `${gameList[i]['title']}`
+        td.textContent = `${gameList[i]['date']}`
+        td1.innerHTML = `<a href=${gameList[i]['link']}>${gameList[i]['title']}</a>`
         td2.textContent = `${gameList[i]['author']}`
         td3.textContent = `${gameList[i]['score']}`
         td4.textContent = `${gameList[i]['platform']}`
+        tr.appendChild(td)
         tr.appendChild(td1)
         tr.appendChild(td2)
         tr.appendChild(td3)
