32be08ecefe213ad1d871b0f808077f75ccd4594
Dado83 <83deadpool@gmail.com>
2020-03-26 17:40:50 +0100

    add search funtion

--- js/skgameindex.js
+++ js/skgameindex.js
@@ -1,32 +1,50 @@
+let jsonFile = fetch('/resources/sk.json')
+let tbody = document.querySelector('tbody')
+
 let form = document.querySelector('form')
 form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
     let title = form.elements['title'].value ? form.elements['title'].value : ''
     let author = form.elements['author'].value ? form.elements['author'].value : ''
     let score = form.elements['score'].value ? form.elements['score'].value : 0
     let platform = document.querySelector('input[name=platform]:checked') ?
         document.querySelector('input[name=platform]:checked').id : ''
-    event.preventDefault()
+
+    let result = gameList.filter((game) => game['score'] >= score)
+    tbody.innerHTML = ''
+    createTable(result)
+})
+
+form.addEventListener('reset', (event) => {
+    tbody.innerHTML = ''
+    createTable(gameList)
 })
 
 
-let jsonFile = fetch('/resources/SKGameIndex.json')
-
-let json = jsonFile.then((response) => response.json().then((data) => {
-    gameList = [];
-    let tbody = document.querySelector('tbody')
+gameList = [];
+jsonFile.then((response) => response.json().then((data) => {
     for (let i = 0; i < data.length; i++) {
         gameList[i] = data[i]
+    }
+    createTable(gameList)
+}))
+
+
+function createTable(list) {
+    document.querySelector('span').innerHTML = list.length
+    for (let i = 0; i < list.length; i++) {
         let tr = document.createElement('tr')
         let td = document.createElement('td')
         let td1 = document.createElement('td')
         let td2 = document.createElement('td')
         let td3 = document.createElement('td')
         let td4 = document.createElement('td')
-        td.textContent = `${gameList[i]['date']}`
-        td1.innerHTML = `<a href=${gameList[i]['link']}>${gameList[i]['title']}</a>`
-        td2.textContent = `${gameList[i]['author']}`
-        td3.textContent = `${gameList[i]['score']}`
-        td4.textContent = `${gameList[i]['platform']}`
+        td.textContent = `${list[i]['date']}`
+        td1.innerHTML = `<a href=${list[i]['link']}>${list[i]['title']}</a>`
+        td2.textContent = `${list[i]['author']}`
+        td3.textContent = `${list[i]['score']}`
+        td4.textContent = `${list[i]['platform']}`
         tr.appendChild(td)
         tr.appendChild(td1)
         tr.appendChild(td2)
@@ -34,5 +52,5 @@ let json = jsonFile.then((response) => response.json().then((data) => {
         tr.appendChild(td4)
         tbody.appendChild(tr)
     }
-}))
+}
 
