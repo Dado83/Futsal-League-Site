31711c0dc377133890d0d278414d843a71cece5b
Dado83 <83deadpool@gmail.com>
2020-03-28 15:06:49 +0100

    set final rankings

--- application/views/finalFour.php
+++ application/views/finalFour.php
@@ -2,19 +2,25 @@
     <h1>Završni turnir</h1>
     <p>Subota, 7. mart 2020. g. - Žepče (2007, 2008, 2010)</p>
     <p>Nedjelja, 8. mart 2020. g. - Maglaj (2006, 2009)</p>
-    <h2>Polufinalne utakmice:</h2>
+    <h2>Plasman:</h2>
     <div class="finals">
         <h3>2010. godište</h3>
         <table>
             <tr>
+                <td>1.</td>
                 <td><img src='/images/logos/<?=$t10p1->id?>.png' alt='grb'><?=$t10p1->team?></td>
-                <td> - </td>
-                <td><?=$t10p4->team?><img src='/images/logos/<?=$t10p4->id?>.png' alt='grb'></td>
             </tr>
             <tr>
+                <td>2.</td>
+                <td><img src='/images/logos/<?=$t10p3->id?>.png' alt='grb'><?=$t10p3->team?></td>
+            </tr>
+            <tr>
+                <td>3.</td>
                 <td><img src='/images/logos/<?=$t10p2->id?>.png' alt='grb'><?=$t10p2->team?></td>
-                <td> - </td>
-                <td><?=$t10p3->team?><img src='/images/logos/<?=$t10p3->id?>.png' alt='grb'></td>
+            </tr>
+            <tr>
+                <td>4.</td>
+                <td><img src='/images/logos/<?=$t10p4->id?>.png' alt='grb'><?=$t10p4->team?></td>
             </tr>
         </table>
     </div>
@@ -22,14 +28,20 @@
         <h3>2009. godište</h3>
         <table>
             <tr>
+                <td>1.</td>
                 <td><img src='/images/logos/<?=$t9p1->id?>.png' alt='grb'><?=$t9p1->team?></td>
-                <td> - </td>
-                <td><?=$t9p4->team?><img src='/images/logos/<?=$t9p4->id?>.png' alt='grb'></td>
             </tr>
             <tr>
+                <td>2.</td>
+                <td><img src='/images/logos/<?=$t9p3->id?>.png' alt='grb'><?=$t9p3->team?></td>
+            </tr>
+            <tr>
+                <td>3.</td>
                 <td><img src='/images/logos/<?=$t9p2->id?>.png' alt='grb'><?=$t9p2->team?></td>
-                <td> - </td>
-                <td><?=$t9p3->team?><img src='/images/logos/<?=$t9p3->id?>.png' alt='grb'></td>
+            </tr>
+            <tr>
+                <td>4.</td>
+                <td><img src='/images/logos/<?=$t9p4->id?>.png' alt='grb'><?=$t9p4->team?></td>
             </tr>
         </table>
     </div>
@@ -37,14 +49,20 @@
         <h3>2008. godište</h3>
         <table>
             <tr>
+                <td>1.</td>
                 <td><img src='/images/logos/<?=$t8p1->id?>.png' alt='grb'><?=$t8p1->team?></td>
-                <td> - </td>
-                <td><?=$t8p4->team?><img src='/images/logos/<?=$t8p4->id?>.png' alt='grb'></td>
             </tr>
             <tr>
+                <td>2.</td>
                 <td><img src='/images/logos/<?=$t8p2->id?>.png' alt='grb'><?=$t8p2->team?></td>
-                <td> - </td>
-                <td><?=$t8p3->team?><img src='/images/logos/<?=$t8p3->id?>.png' alt='grb'></td>
+            </tr>
+            <tr>
+                <td>3.</td>
+                <td><img src='/images/logos/<?=$t8p3->id?>.png' alt='grb'><?=$t8p3->team?></td>
+            </tr>
+            <tr>
+                <td>4.</td>
+                <td><img src='/images/logos/<?=$t8p4->id?>.png' alt='grb'><?=$t8p4->team?></td>
             </tr>
         </table>
     </div>
@@ -52,14 +70,20 @@
         <h3>2007. godište</h3>
         <table>
             <tr>
+                <td>1.</td>
                 <td><img src='/images/logos/<?=$t7p1->id?>.png' alt='grb'><?=$t7p1->team?></td>
-                <td> - </td>
-                <td><?=$t7p4->team?><img src='/images/logos/<?=$t7p4->id?>.png' alt='grb'></td>
             </tr>
             <tr>
+                <td>2.</td>
                 <td><img src='/images/logos/<?=$t7p2->id?>.png' alt='grb'><?=$t7p2->team?></td>
-                <td> - </td>
-                <td><?=$t7p3->team?><img src='/images/logos/<?=$t7p3->id?>.png' alt='grb'></td>
+            </tr>
+            <tr>
+                <td>3.</td>
+                <td><img src='/images/logos/<?=$t7p4->id?>.png' alt='grb'><?=$t7p4->team?></td>
+            </tr>
+            <tr>
+                <td>4.</td>
+                <td><img src='/images/logos/<?=$t7p3->id?>.png' alt='grb'><?=$t7p3->team?></td>
             </tr>
         </table>
     </div>
@@ -67,14 +91,20 @@
         <h3>2006. godište</h3>
         <table>
             <tr>
-                <td><img src='/images/logos/<?=$t6p1->id?>.png' alt='grb'><?=$t6p1->team?></td>
-                <td> - </td>
-                <td><?=$t6p4->team?><img src='/images/logos/<?=$t6p4->id?>.png' alt='grb'></td>
+                <td>1.</td>
+                <td><img src='/images/logos/<?=$t6p2->id?>.png' alt='grb'><?=$t6p2->team?></td>
             </tr>
             <tr>
-                <td><img src='/images/logos/<?=$t6p2->id?>.png' alt='grb'><?=$t6p2->team?></td>
-                <td> - </td>
-                <td><?=$t6p3->team?><img src='/images/logos/<?=$t6p3->id?>.png' alt='grb'></td>
+                <td>2.</td>
+                <td><img src='/images/logos/<?=$t6p4->id?>.png' alt='grb'><?=$t6p4->team?></td>
+            </tr>
+            <tr>
+                <td>3.</td>
+                <td><img src='/images/logos/<?=$t6p1->id?>.png' alt='grb'><?=$t6p1->team?></td>
+            </tr>
+            <tr>
+                <td>4.</td>
+                <td><img src='/images/logos/<?=$t6p3->id?>.png' alt='grb'><?=$t6p3->team?></td>
             </tr>
         </table>
     </div>
