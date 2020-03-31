091350a2a549005cae186b94be7c4d77afadd877
Dado83 <83deadpool@gmail.com>
2020-03-31 16:20:52 +0200

    Set session&record visitors

--- application/controllers/Sk.php
+++ application/controllers/Sk.php
@@ -3,9 +3,44 @@
 class Sk extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('DBModel', '', true);
+        $this->setSession();
+    }
+
+    private function setSession($role = '')
+    {
+        $sessionData = [
+            'ip' => $this->input->ip_address(),
+            'mobile' => $this->agent->mobile(),
+            'robot' => $this->agent->robot(),
+            'platform' => $this->agent->platform(),
+            'browser' => $this->agent->browser(),
+            'version' => $this->agent->version(),
+            'userAgent' => $this->agent->agent_string(),
+        ];
+
+        $sessionStartTime = time();
+        $sessionData['startTime'] = $sessionStartTime;
+
+        $cookie = get_cookie('visited');
+        if ($cookie) {
+            $sessionData['newVisitor'] = 0;
+        } else {
+            set_cookie('visited', '1', 60 * 60 * 24 * 30 * 4);
+            $sessionData['newVisitor'] = 1;
+        }
+
+        $sessionData['role'] = $role;
+        $this->session->set_userdata($sessionData);
+    }
+
     public function index()
     {
         $this->load->view('sktestplay');
+        $this->DBModel->setVisitor();
     }
 
 }
